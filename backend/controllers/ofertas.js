fb19a961b23ac28a18cc6433337ef10e21547047
Asier Del Cid Pérez <103944663+AsierDelCidPerez@users.noreply.github.com>
2024-09-05 16:28:51 +0200

    05/09/2024

--- backend/controllers/ofertas.js
+++ backend/controllers/ofertas.js
@@ -1,9 +1,19 @@
 
 const ofertasRouter = require('express').Router()
 const Oferta = require('../model/Oferta')
+const Grupo = require('../model/Grupo')
 
-ofertasRouter.post("/addOferta", async(req, res) => {
-    Ofert
+const validador = async (email, origen, destino){
+    const res1 = await Grupo.findOne({name: origen})
+    const res2 = await Grupo.findOne({name: destino})
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+    return regex.test(email) & res1.length > 0 & res2.length > 0
+}
+
+ofertasRouter.post("/addOferta", async(req, res) => { // En la BBDD se almacenan las ofertas según el interés de las personas
+    const {email, origen, destino} = req.body
+    
+    Oferta.
 })
 
 export default ofertasRouter
